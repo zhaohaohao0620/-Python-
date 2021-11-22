0942e8c444e1220ec051f358374ef15862575ee5
zhaohaohao <17631825626@163.com>
2021-11-22 20:33:45 +0800

    webservice

--- 05_wb/client.py
+++ 05_wb/client.py
@@ -1,16 +1,17 @@
 from suds.client import Client
 
+
 def ClientTest():
     url = 'http://127.0.0.1:8080/?wsdl'
 
-    req = {}
-    req['demo1'] = 'client test'
+    req = {'demo1': 'client test'}
 
     client = Client(url)
     rsp = client.service.Test1(req)
     print(rsp)
     print(str(client.last_sent()))
     print(str(client.last_received()))
+
 
 if __name__ == '__main__':
     ClientTest()
