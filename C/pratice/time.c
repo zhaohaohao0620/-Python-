1eaa4af1804c1af470e21f63955a4126c2122e7a
zhaohaohao <854724495@qq.com>
2022-06-30 21:58:13 +0800

    获取时间

--- C/pratice/time.c
+++ C/pratice/time.c
@@ -3,6 +3,19 @@
 #include <time.h>
 #include <sys/time.h>
 
+
+/* struct tm {
+  int tm_sec;			//秒[0-59]
+  int tm_min;			//分[0-59]
+  int tm_hour;			//时[0-23]
+  int tm_mday;			//日[1-31]
+  int tm_mon;			//月份[0-11]，0代表一月
+  int tm_year;			//年份，需要加上1900
+  int tm_wday;			//星期[0-6]，0代表星期天
+  int tm_yday;			//从每年1月1日开始的天数[0-365]，0代表1月1日
+}; */
+
+
 int main()
 {
     // timestamp
